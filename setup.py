dc38f1c87cc585c8fefbffda0f49bf8e82d26832
Rahul <rahulksree2005@gmail.com>
2025-02-21 22:29:55 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot', 	# Name of the package
+    version = '0.0.1', 		    # Version of the package
+    author = 'Rahul Kummara',
+    author_email = 'rahulsree2005@gmail.com',
+    install_requires = [],
+    packages = find_packages()
+)
